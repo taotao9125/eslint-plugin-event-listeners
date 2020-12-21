144b12c1754a4b5a93e7b69d8e45ab9ee361ce45
blue.yang <blue.yang@qunar.com>
2020-12-21 15:30:44 +0800

    feat: add some test case

--- tests/lib/rules/remove-device-event-listener.js
+++ tests/lib/rules/remove-device-event-listener.js
@@ -8,29 +8,54 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/remove-event-listener"),
-
-    RuleTester = require("eslint").RuleTester;
+var rule = require('../../../lib/rules/remove-device-event-listener');
+var lint = require("eslint");
+var RuleTester = lint.RuleTester;
 
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-var ruleTester = new RuleTester();
+var ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 2018
+    }
+});
+
 ruleTester.run("remove-event-listener", rule, {
 
+    // good case
     valid: [
-
         // give me some code that won't trigger a warning
+        `
+            class C extends Component {
+                componentDidMount() {
+                    Dimensions.addEventListener('change', this.sizeChange);
+                }
+                componentWillUnmount() {
+                    Dimensions.removeEventListener('change', this.sizeChange);
+                }
+            }
+        `.trim()
     ],
 
+    // bad case
     invalid: [
         {
-            code: "please remove your listener",
+            code: `
+                class C extends Component {
+                    componentDidMount() {
+                        BackHandler.addEventListener('hardwareBackPress', this.sizeChange.bind(this));
+                    }
+                    componentWillUnmount() {
+                        BackHandler.addEventListener('hardwareBackPress', this.sizeChange.bind(this));
+                    }
+                }
+            `.trim(),
             errors: [{
                 message: "Fill me in.",
-                type: "Me too"
+                type: 'CallExpression'
             }]
         }
     ]
